894d9a1e7f928ff1538063d8f25b06b51e17cb17
kkinge79 <kingerykagan@gmail.com>
2021-10-15 11:50:06 -0500

    fixed winner message && added images for slots

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
 // /*-------------------------------- Constants --------------------------------*/
-// const cherry = 'https://e7.pngegg.com/pngimages/764/629/;png-clipart-cherry-fruit-texas-hold-em-slot-machine-unity-poker-icon-cherry-game-food.png';
-// const diamond = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT48T-uQu6rkAK8Mgu4cj3o8kir3sT1tcJ2Qw&usqp=CAU';
-// const bell = 'https://www.pinclipart.com/picdir/middle/571-5714183_slot-machine-bell-png-clipart.png';
-// const seven = 'https://cdn3.iconfinder.com/data/icons/slot-machine-symbols-filled-outline/256/7-512.png';
+const cherry = '../images/cherry.jpeg';
+const diamond = '../images/diamond.jpeg';
+const bell = '../images/bell.png';
+const seven = '../images/seven.jpeg';
 
-// const slotsReel = 4;
+const slotsReel = 4;
 
 // /*-------------------------------- Variables --------------------------------*/
 
 
-// const arrayOfChoices = [cherry, diamond, bell, seven]
+const arrayOfChoices = [cherry, diamond, bell, seven]
 
 // /*------------------------ Cached Element References ------------------------*/
 
@@ -24,7 +24,7 @@ const titleEl = document.querySelector('#title');
 //generates a random number between 1-4
 //adds plus 1 to all numbers so instead of 0-3. it chooses 1-4
 function getRandomNumber() {
-  return Math.floor(Math.random() * 4) + 1;
+  return Math.floor(Math.random() * 4);
 }
 
 
@@ -39,9 +39,10 @@ function spin() {
   const num2 = getRandomNumber();
   const num3 = getRandomNumber();
 // assaigning each slow with a randomly generated number
-slotOne.innerHTML = `${num1}`;
-slotTwo.innerHTML =  `${num2}`;
-slotThree.innerHTML =  `${num3}`;
+//!
+  slotOne.innerHTML = `<img class="imgOne" src="${arrayOfChoices[num1]}">`;
+  slotTwo.innerHTML =  `<img class="imgTwo" src="${arrayOfChoices[num2]}">`;
+  slotThree.innerHTML =  `<img class="imgThree" src="${arrayOfChoices[num3]}">` ;
 //determine if there is a winner, if yes show the WINNER message, if not, hide message
 if (num1 === num2 && num1 === num3) {
   showMessage();
